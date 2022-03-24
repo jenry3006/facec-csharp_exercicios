e8c205af6bf18ba98a1bf4769c695d77657cc4cf
jenry <>
2022-03-23 22:03:44 -0300

    enviando atividades

---
+++ Exercicio.7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacecExercicioListas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Por favor informe seu nome");
+                var nome = Console.ReadLine();
+
+                Console.WriteLine("Informe sua data de nascimento agora!! No formato dd/mm/yyyy");
+                var dataNascimento = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine(ObterMensagemFaixaEtaria(nome, dataNascimento));
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);  
+            } finally
+            {
+                Console.ReadLine();           
+            }   
+
+        }
+
+        private static string ObterMensagemFaixaEtaria(string nome, DateTime dataNascimento)
+        {
+            var idade = DateTime.Today.Year - dataNascimento.Year;
+            if (idade >=60)
+                return $"{nome} é velho";
+            else if (idade >= 20 && idade <= 59)
+                return $"{nome} é adulto";
+            return $"{nome} é jovem";
+        }
+    }
+}
---
+++ Exercicio.8/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio._8
+{
+    internal class Program
+    {
+        private static readonly char[] _vogais = new char[]
+        {
+            'a', 'e', 'i', 'o','u',
+        };
+
+        //private const char _a = 'a';    
+       // private const char _e = 'e';    
+       // private const char _i = 'i';    
+       // private const char _o = 'o';    
+       // private const char _u = 'u';    
+
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Digite seu nome");
+                var nome = Console.ReadLine().ToLower();
+                var count = 0;
+
+                for(int i=0; i< nome.Length; i++)
+                //foreach(var letra in nome)
+                {
+                    var letra = nome[i];
+
+                    /*if (!_a.Equals(letra) &&
+                        !_e.Equals(letra) &&
+                        !_i.Equals(letra) &&
+                        !_o.Equals(letra) &&
+                        !_u.Equals(letra))
+                        count++;*/
+                    if(!_vogais.Contains(letra))
+                        count++;    
+                }
+
+                Console.WriteLine($"Seu nome possui {count}" + "consoantes");
+
+            } catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);  
+            }
+            finally
+            {
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
